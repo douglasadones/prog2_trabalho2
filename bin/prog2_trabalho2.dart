121c73d25885079e8cae0691bcabc042b91a5b7a
Douglas Nascimento <contato.douglas.ad@gmail.com>
2023-11-22 09:07:18 -0300

    adicionando

---
+++ bin/prog2_trabalho2.dart
@@ -0,0 +1,5 @@
+import 'package:prog2_trabalho2/prog2_trabalho2.dart' as prog2_trabalho2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prog2_trabalho2.calculate()}!');
+}
---
+++ test/prog2_trabalho2_test.dart
@@ -0,0 +1,8 @@
+import 'package:prog2_trabalho2/prog2_trabalho2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
